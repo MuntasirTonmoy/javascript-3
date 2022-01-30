fe4d69e326a6bf95ac09aee5647d3e04073163a2
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-30 22:48:59 +0600

    for loop in js

---
+++ lecture-4-for-loop-in-javascript.js
@@ -0,0 +1,18 @@
+//in for loop loopVariable is declared inside the loop then condition then increment or decrement
+/*
+
+*syntax
+*   for( loopVariable; condition; increament){
+    loop statement;
+    }
+
+*/
+// in for loop the loop variable is i
+
+for (var i = 1; i < 8; i++) //no semecolon in last statement 
+{
+    console.log(i);
+    console.log('vai roast den, please!');
+
+}
+
