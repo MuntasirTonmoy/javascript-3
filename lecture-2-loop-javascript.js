b6d410dfd86fdcc234353e2640aa67ebc2cc24f3
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-30 22:02:15 +0600

    while loop in javascript

---
+++ lecture-2-loop-javascript.js
@@ -0,0 +1,11 @@
+// javascript while loop
+
+var roastGiven = 0; //loop variable
+
+//* the syntax of while loop is while( condition ){ statement};
+
+while (roastGiven < 7) {
+    console.log(roastGiven);
+    console.log('vai roast den');
+    roastGiven++; // if value isn't increase or decrease the the loop becomes infinite. cause value stay same.
+}
