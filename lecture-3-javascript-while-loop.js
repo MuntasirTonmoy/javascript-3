e33f4fb33890ecc9648bb01c5e41c2d85738d0f2
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-30 22:29:42 +0600

    while loop example printing numbers even numbers odd numbers

---
+++ lecture-3-javascript-while-loop.js
@@ -0,0 +1,28 @@
+// for while loop we need
+//* loop variable 
+//* condition
+//* work that is need to do again and again means statement
+//* increment or decrement means we have to change the variable value.
+
+// show number 1-50
+
+var num1 = 1;
+while (num1 <= 50) {
+    console.log(num1);
+    num1++;
+}
+
+// show odd number from 1-50
+var odd = 1;
+while (odd <= 50) {
+    console.log(odd);
+    odd += 2;
+}
+
+// show even number from 1-50
+
+var even = 2;
+while (even <= 50) {
+    console.log(even);
+    even += 2;
+}
