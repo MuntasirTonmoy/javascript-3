2571231f4e5259778593dccc7ab95f3e37124fdd
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-30 23:10:19 +0600

    loop array

---
+++ lecture-6-loop-recap-and-practice.js
@@ -0,0 +1,19 @@
+// array in loop
+
+var num = [12, 23, 34, 56, 78];
+
+//* for loop
+
+// for (var i = 0; i < num.length; i++) {
+//     console.log(num[i]);
+// }
+
+//* while loop
+
+// var i = 0;
+// while (i < num.length) {
+//     console.log(num[i]);
+//     i++;
+// }
+
+// TODO. javascript loop through an array.
